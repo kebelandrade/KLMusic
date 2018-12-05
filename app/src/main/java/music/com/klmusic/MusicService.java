bca11a748d946375156a5664a1adb627a4613827
kebelandrade <kebelandrade@github.com>
2018-12-04 18:20:58 -0600

    Modificación en la clase MusicService.java

--- app/src/main/java/music/com/klmusic/MusicService.java
+++ app/src/main/java/music/com/klmusic/MusicService.java
@@ -1,8 +1,14 @@
 package music.com.klmusic;
 
 import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
 
 public class MusicService extends Service {
-
+    @androidx.annotation.Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 }
 
