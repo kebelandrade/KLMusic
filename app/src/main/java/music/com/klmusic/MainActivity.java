3f7d82da998d67dd178859d71147723ac4d6f214
kebelandrade <kebelandrade@github.com>
2018-12-07 16:57:04 -0600

    Casi completo

--- app/src/main/java/music/com/klmusic/MainActivity.java
+++ app/src/main/java/music/com/klmusic/MainActivity.java
@@ -8,27 +8,39 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import android.net.Uri;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String CANAL_PRINCIPAL = "principal";
-
-    Button mBtnIniciar;
+    private ArrayList<Song> songList;
+    private ListView songView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        songView = (ListView)findViewById(R.id.song_list);
+        songList = new ArrayList<Song>();
         registrarCanalPrincipal();
         regis();
-        mBtnIniciar = findViewById(R.id.activity_main_btn_iniciar);
-        mBtnIniciar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, MusicService.class);
-                startService(intent);
+        Intent intent = new Intent(MainActivity.this, MusicService.class);
+        startService(intent);
+        getSongList();
+        Collections.sort(songList, new Comparator<Song>(){
+            public int compare(Song a, Song b){
+                return a.getTitle().compareTo(b.getTitle());
             }
         });
+
+        SongAdapter songAdt = new SongAdapter(this, songList);
+        songView.setAdapter(songAdt);
     }
 
     private void registrarCanalPrincipal() {
@@ -52,6 +64,29 @@ public class MainActivity extends AppCompatActivity {
             channel.setDescription(descripcion);
             NotificationManager notificationManager = getSystemService(NotificationManager.class);
             notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    public void getSongList() {
+        ContentResolver musicResolver = getContentResolver();
+        Uri musicUri = android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        Cursor musicCursor = musicResolver.query(musicUri, null, null, null, null);
+        if(musicCursor!=null && musicCursor.moveToFirst()){
+            //get columns
+            int titleColumn = musicCursor.getColumnIndex
+                    (android.provider.MediaStore.Audio.Media.TITLE);
+            int idColumn = musicCursor.getColumnIndex
+                    (android.provider.MediaStore.Audio.Media._ID);
+            int artistColumn = musicCursor.getColumnIndex
+                    (android.provider.MediaStore.Audio.Media.ARTIST);
+            //add songs to list
+            do {
+                long thisId = musicCursor.getLong(idColumn);
+                String thisTitle = musicCursor.getString(titleColumn);
+                String thisArtist = musicCursor.getString(artistColumn);
+                songList.add(new Song(thisId, thisTitle, thisArtist));
+            }
+            while (musicCursor.moveToNext());
         }
     }
 }
