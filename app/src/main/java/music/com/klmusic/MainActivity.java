43eb6ef47e228dea82dde82ede01758cd8e509cb
Ledinalvarado <ledinalvarado18@gmail.com>
2018-12-07 19:32:08 -0600

    Cambio de color en la actividad principal y agregué el logo

--- app/src/main/java/music/com/klmusic/MainActivity.java
+++ app/src/main/java/music/com/klmusic/MainActivity.java
@@ -1,92 +1,18 @@
 package music.com.klmusic;
 
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.content.Intent;
-import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import android.net.Uri;
-import android.content.ContentResolver;
-import android.database.Cursor;
-import android.widget.ListView;
+//import android.*;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String CANAL_PRINCIPAL = "principal";
-    private ArrayList<Song> songList;
-    private ListView songView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        songView = (ListView)findViewById(R.id.song_list);
-        songList = new ArrayList<Song>();
-        registrarCanalPrincipal();
-        regis();
-        Intent intent = new Intent(MainActivity.this, MusicService.class);
-        startService(intent);
-        getSongList();
-        Collections.sort(songList, new Comparator<Song>(){
-            public int compare(Song a, Song b){
-                return a.getTitle().compareTo(b.getTitle());
-            }
-        });
 
-        SongAdapter songAdt = new SongAdapter(this, songList);
-        songView.setAdapter(songAdt);
-    }
-
-    private void registrarCanalPrincipal() {
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            String nombre = getString(R.string.notif_musica);
-            String descripcion = "Categoria que controla la reporoduccion de musica";
-            int importancia = NotificationManager.IMPORTANCE_HIGH;
-            NotificationChannel channel = new NotificationChannel(CANAL_PRINCIPAL, nombre, importancia);
-            channel.setDescription(descripcion);
-            NotificationManager notificationManager = getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
-        }
-    }
-
-    private void regis(){
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            String nombre = "Hola";
-            String descripcion = "Categoria que controla la reporoduccion de musica";
-            int importancia = NotificationManager.IMPORTANCE_LOW;
-            NotificationChannel channel = new NotificationChannel("Hola", nombre, importancia);
-            channel.setDescription(descripcion);
-            NotificationManager notificationManager = getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
-        }
-    }
-
-    public void getSongList() {
-        ContentResolver musicResolver = getContentResolver();
-        Uri musicUri = android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
-        Cursor musicCursor = musicResolver.query(musicUri, null, null, null, null);
-        if(musicCursor!=null && musicCursor.moveToFirst()){
-            //get columns
-            int titleColumn = musicCursor.getColumnIndex
-                    (android.provider.MediaStore.Audio.Media.TITLE);
-            int idColumn = musicCursor.getColumnIndex
-                    (android.provider.MediaStore.Audio.Media._ID);
-            int artistColumn = musicCursor.getColumnIndex
-                    (android.provider.MediaStore.Audio.Media.ARTIST);
-            //add songs to list
-            do {
-                long thisId = musicCursor.getLong(idColumn);
-                String thisTitle = musicCursor.getString(titleColumn);
-                String thisArtist = musicCursor.getString(artistColumn);
-                songList.add(new Song(thisId, thisTitle, thisArtist));
-            }
-            while (musicCursor.moveToNext());
-        }
     }
 }
+//ctrl+k, para hacer commit, ctrl+t para actualizar projecto, ctrl+shift+k para hacer el push
