405b4253b3d9972a9d4f260fe3834ab77822cc5a
kebelandrade <kebelandrade@github.com>
2018-12-10 15:42:06 -0600

    Ya está todo.

---
+++ app/src/main/java/music/com/klmusic/SongAdapter.java
@@ -0,0 +1,58 @@
+package music.com.klmusic;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import java.util.ArrayList;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class SongAdapter extends BaseAdapter {
+
+    private ArrayList<Song> songs;
+    private LayoutInflater songInf;
+
+    @Override
+    public int getCount() {
+        return songs.size();
+    }
+
+    @Override
+    public Object getItem(int arg0) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public long getItemId(int arg0) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public View getView(int arg0, View arg1, ViewGroup arg2) {
+        //map to song layout
+        ViewGroup parent = arg2;
+        LinearLayout songLay = (LinearLayout)songInf.inflate
+                (R.layout.song, parent, false);
+        //get title and artist views
+        TextView songView = (TextView)songLay.findViewById(R.id.song_title);
+        TextView artistView = (TextView)songLay.findViewById(R.id.song_artist);
+        //get song using position
+        int position = arg0;
+        Song currSong = songs.get(position);
+        //get title and artist strings
+        songView.setText(currSong.getTitle());
+        artistView.setText(currSong.getArtist());
+        //set position as tag
+        songLay.setTag(position);
+        return songLay;
+    }
+
+    public SongAdapter(Context c, ArrayList<Song> theSongs){
+        songs=theSongs;
+        songInf=LayoutInflater.from(c);
+    }
+}
